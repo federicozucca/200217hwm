d9bc0f731074b5761dbd95a791ce4c329fb0dd84
Federico Zucca <zucca.federico@gmail.com>
2017-02-21 09:13:04 +0000

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+Show.delete_all()
+
+Show.create({
+  title: "The Great British Bake Off",
+  series: 7,
+  description: "Master bakers Mary & Paul and the incomparable presenting duo of Mel & Sue are back for another interesting series about baking cakes.",
+  image: "placeholder.jpg",
+  programmeID: "b013pqnm" 
+})
+
+Show.create({
+title: "MasterChef Italia",
+series: 6,
+description: "Chef Cracco, Cannavaciulo, etc....",
+image: "http://nst.sky.it/content/dam/static/contentimages/original/sezioni/skyuno/news/2017/01/02/masterchef_ep5/03-masterchef-italia-ep5.jpg/_jcr_content/renditions/cq5dam.web.738.462.jpeg",
+programmeID: "msfas"
+})
+
+Show.create({
+title: "Prison Break",
+series: 4,
+description: "Escape from a prison :D",
+image: "http://prisonbreakmanhunt.com/wp-content/uploads/2016/05/prison-break.jpg",
+programmeID: "prsd2"
+})
